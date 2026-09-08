30e142ad00d87a47b5ccd6c2ffe3a546ec361f9e
sebastian07A <juansebastiananguloperez1@gmail.com>
2026-09-07 21:08:44 -0500

    servidor funcionando sin configuración Package.json

---
+++ index.js
@@ -0,0 +1,15 @@
+//importacion del paquete de express, tradicional(require)
+const miExpress = require("express")
+
+//creacion de mi aplicacion de express
+const miApp = miExpress()
+const miPuerto = 3333
+
+//endpoint raiz, no tiene ruta
+miApp.get("/",(req, res)=>{
+    res.send(`<h1>Api Rest Productos la 80</h1 > `)})
+
+//link del servidor, por donde se escucha las peticiones del usuario.
+miApp.listen(miPuerto, ()=>{
+console.log(`SERVIDOR: http://localhost:miPuerto`)
+})
